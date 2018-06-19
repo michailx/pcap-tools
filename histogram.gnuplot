2609c1385fa4c7ba62006d943ac9c17c61dc6964
Michail Xirouchakis <michail.xirouchakis@gmail.com>
2018-06-19 16:04:01 +0200

    [histogram] Gnuplot graph shows distribution's statistical properties
    Now the histogram graph also shows the distribution's statistical
    properties, i.e., mean, standard deviation, skewness and kurtosis,
    under the graph title.
    
    Signed-off-by: michailx <michail.xirouchakis@gmail.com>

--- histogram.gnuplot
+++ histogram.gnuplot
@@ -5,7 +5,8 @@
 
 INTPUT_DATA=ARG1  # the data file which I will read for data
 OUTPUT_PNG=ARG2   # the filename to use when storing the generated plot (in png format)
-X_AXIS_UNIT=ARG3  # unit for x-axis, e.g., bps
+X_STAT_PROP=ARG3  # statistical properties of series (x-axis)
+X_AXIS_UNIT=ARG4  # unit for x-axis, e.g., bps
 
 set terminal png size 1024,768
 set output OUTPUT_PNG
@@ -13,7 +14,7 @@ set output OUTPUT_PNG
 # styles
 set style line 1 lc rgb '#dc3912' lt 1 lw 1 pt 9 ps 1.25  # Dark Red color
 
-set title "Histogram of observed traffic load (".X_AXIS_UNIT.")"
+set title "Histogram of observed traffic load (".X_AXIS_UNIT.")\n".X_STAT_PROP."
 set xlabel "Traffic load (".X_AXIS_UNIT.")"
 set xtic rotate by -45 scale 0
 set ylabel "Count"
--- plot_hist.sh
+++ plot_hist.sh
@@ -21,9 +21,10 @@ OUTPUT_FILE=${IF_ARRAY[0]}.histogram.${X_AXIS_UNIT}  # Example result: uliege_in
 
 # Step 1: Read the series from the input data file, compute the histogram, and output the data to a new file
 echo Exporting histogram data to ${DAT_DIRECTORY}/${OUTPUT_FILE}.dat
-python ./create_histogram.py ${DAT_DIRECTORY}/${INPUT_FILE} ${DAT_DIRECTORY}/${OUTPUT_FILE}.dat ${OUTPUT_NUM_SCALE}
+CMD=`python ./create_histogram.py ${DAT_DIRECTORY}/${INPUT_FILE} ${DAT_DIRECTORY}/${OUTPUT_FILE}.dat ${OUTPUT_NUM_SCALE}`
+echo ${CMD}
 
 # Step 2: Read the histogram data file and generate a plot
 echo Exporting histogram graph to ${PNG_DIRECTORY}/${OUTPUT_FILE}.png
 gnuplot -c histogram.gnuplot \
-${DAT_DIRECTORY}/${OUTPUT_FILE}.dat ${PNG_DIRECTORY}/${OUTPUT_FILE}.png ${X_AXIS_UNIT}
+${DAT_DIRECTORY}/${OUTPUT_FILE}.dat ${PNG_DIRECTORY}/${OUTPUT_FILE}.png ${CMD} ${X_AXIS_UNIT}
